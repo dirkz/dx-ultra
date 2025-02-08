c24cfca94036a285acb4672ae39f6df41c61f298
Dirk Zimmermann <me@dirkz.com>
2025-02-08 11:28:09 +0100

    Least amount to a usable device

--- main/DXUltra.cpp
+++ main/DXUltra.cpp
@@ -5,6 +5,28 @@ namespace dxultra
 
 void DXUltra::OnInit(HWND hwnd, UINT width, UINT height)
 {
+#if defined(_DEBUG)
+    // Enable the D3D12 debug layer.
+    {
+
+        ComPtr<ID3D12Debug> debugController;
+        if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+        {
+            debugController->EnableDebugLayer();
+        }
+    }
+#endif
+
+    ComPtr<IDXGIFactory4> factory;
+    ThrowIfFailed(CreateDXGIFactory1(IID_PPV_ARGS(&factory)));
+
+    // EnumAdapters1 returns the primary adapter first, I assume
+    // this is the primary device, for now.
+    ComPtr<IDXGIAdapter1> adapter;
+    ThrowIfFailed(factory->EnumAdapters1(0, adapter.GetAddressOf()));
+
+    ThrowIfFailed(D3D12CreateDevice(adapter.Get(), D3D_FEATURE_LEVEL_11_0,
+                                    IID_PPV_ARGS(m_device.GetAddressOf())));
 }
 
 void DXUltra::OnUpdate()
--- main/DXUltra.h
+++ main/DXUltra.h
@@ -12,6 +12,9 @@ struct DXUltra : WindowCallback
     void OnInit(HWND hwnd, UINT width, UINT height) override;
     void OnUpdate() override;
     void OnRender() override;
+
+  private:
+    ComPtr<ID3D12Device> m_device;
 };
 
 } // namespace dxultra
