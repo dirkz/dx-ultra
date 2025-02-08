8e0617effba52dc919e743024081736d6be01c47
Dirk Zimmermann <me@dirkz.com>
2025-02-08 20:21:51 +0100

    Give the fence a handle

--- main/dxutil/Fence.cpp
+++ main/dxutil/Fence.cpp
@@ -9,6 +9,16 @@ Fence::Fence(ID3D12Device4 *pDevice) : m_value{InitialValue}
 {
     ThrowIfFailed(pDevice->CreateFence(InitialValue, D3D12_FENCE_FLAG_NONE,
                                        IID_PPV_ARGS(m_fence.GetAddressOf())));
+    m_event = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+    if (!m_event)
+    {
+        CheckLastError();
+    }
+}
+
+Fence::~Fence()
+{
+    CloseHandle(m_event);
 }
 
 } // namespace dxultra
--- main/dxutil/Fence.h
+++ main/dxutil/Fence.h
@@ -8,10 +8,12 @@ namespace dxultra
 struct Fence
 {
     Fence(ID3D12Device4 *pDevice);
+    ~Fence();
 
   private:
     UINT64 m_value;
     ComPtr<ID3D12Fence> m_fence;
+    HANDLE m_event;
 };
 
 } // namespace dxultra
