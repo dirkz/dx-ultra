66a9443db680cf16ecb6e0fb5234e2cdd9585bfd
Dirk Zimmermann <me@dirkz.com>
2025-02-09 20:32:03 +0100

    That Upload() could work

--- main/dxutil/DefaultBufferUploader.cpp
+++ main/dxutil/DefaultBufferUploader.cpp
@@ -13,4 +13,41 @@ DefaultBufferUploader::DefaultBufferUploader(ID3D12Device4 *pDevice,
                                                   IID_PPV_ARGS(m_commandAllocator.GetAddressOf())));
 }
 
+ComPtr<ID3D12Resource1> DefaultBufferUploader::Upload(const void *pData, UINT64 size)
+{
+
+    ComPtr<ID3D12Resource1> defaultBuffer;
+    CD3DX12_HEAP_PROPERTIES heapPropertiesDefault{D3D12_HEAP_TYPE_DEFAULT};
+    CD3DX12_RESOURCE_DESC desc = CD3DX12_RESOURCE_DESC::Buffer(size);
+    ThrowIfFailed(m_device->CreateCommittedResource(&heapPropertiesDefault, D3D12_HEAP_FLAG_NONE,
+                                                    &desc, D3D12_RESOURCE_STATE_COMMON, nullptr,
+                                                    IID_PPV_ARGS(defaultBuffer.GetAddressOf())));
+
+    ComPtr<ID3D12Resource1> uploadBuffer;
+    CD3DX12_HEAP_PROPERTIES heapPropertiesUpload{D3D12_HEAP_TYPE_UPLOAD};
+    ThrowIfFailed(m_device->CreateCommittedResource(
+        &heapPropertiesUpload, D3D12_HEAP_FLAG_NONE, &desc, D3D12_RESOURCE_STATE_GENERIC_READ,
+        nullptr, IID_PPV_ARGS(uploadBuffer.GetAddressOf())));
+
+    D3D12_SUBRESOURCE_DATA subResourceData{};
+    subResourceData.pData = pData;
+    subResourceData.RowPitch = size;
+    subResourceData.SlicePitch = size;
+
+    auto transition1 = CD3DX12_RESOURCE_BARRIER::Transition(
+        defaultBuffer.Get(), D3D12_RESOURCE_STATE_COMMON, D3D12_RESOURCE_STATE_COPY_DEST);
+    m_commandList->ResourceBarrier(1, &transition1);
+
+    UpdateSubresources<1>(m_commandList, defaultBuffer.Get(), uploadBuffer.Get(), 0, 0, 1,
+                          &subResourceData);
+
+    auto transition2 = CD3DX12_RESOURCE_BARRIER::Transition(
+        defaultBuffer.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_COMMON);
+    m_commandList->ResourceBarrier(1, &transition2);
+
+    m_uploadBuffers.push_back(uploadBuffer);
+
+    return defaultBuffer;
+}
+
 } // namespace dxultra
--- main/dxutil/DefaultBufferUploader.h
+++ main/dxutil/DefaultBufferUploader.h
@@ -13,12 +13,15 @@ struct DefaultBufferUploader
     DefaultBufferUploader(ID3D12Device4 *pDevice, ID3D12CommandQueue *pCommandQueue,
                           ID3D12GraphicsCommandList *pCommandList);
 
+    ComPtr<ID3D12Resource1> Upload(const void *pData, UINT64 size);
+
   private:
     ID3D12Device4 *m_device;
     ID3D12CommandQueue *m_commandQueue;
     ID3D12GraphicsCommandList *m_commandList;
     ComPtr<ID3D12CommandAllocator> m_commandAllocator;
     Fence m_fence;
+    std::vector<ComPtr<ID3D12Resource1>> m_uploadBuffers;
 };
 
 } // namespace dxultra
