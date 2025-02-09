7e8e7c0db794849db1569d507ad2429185983544
Dirk Zimmermann <me@dirkz.com>
2025-02-09 19:58:41 +0100

    DefaultBufferUploader() with maybe everything needed

--- main/dxutil/DefaultBufferUploader.cpp
+++ main/dxutil/DefaultBufferUploader.cpp
@@ -3,4 +3,14 @@
 namespace dxultra
 {
 
+DefaultBufferUploader::DefaultBufferUploader(ID3D12Device4 *pDevice,
+                                             ID3D12CommandQueue *pCommandQueue,
+                                             ID3D12GraphicsCommandList *pCommandList)
+    : m_device{pDevice}, m_commandQueue{pCommandQueue}, m_commandList{pCommandList},
+      m_fence{pDevice}
+{
+    ThrowIfFailed(pDevice->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT,
+                                                  IID_PPV_ARGS(m_commandAllocator.GetAddressOf())));
 }
+
+} // namespace dxultra
--- main/dxutil/DefaultBufferUploader.h
+++ main/dxutil/DefaultBufferUploader.h
@@ -2,11 +2,23 @@
 
 #include "stdafx.h"
 
+#include "Fence.h"
+
 namespace dxultra
 {
 
 struct DefaultBufferUploader
 {
+
+    DefaultBufferUploader(ID3D12Device4 *pDevice, ID3D12CommandQueue *pCommandQueue,
+                          ID3D12GraphicsCommandList *pCommandList);
+
+  private:
+    ID3D12Device4 *m_device;
+    ID3D12CommandQueue *m_commandQueue;
+    ID3D12GraphicsCommandList *m_commandList;
+    ComPtr<ID3D12CommandAllocator> m_commandAllocator;
+    Fence m_fence;
 };
 
 } // namespace dxultra
