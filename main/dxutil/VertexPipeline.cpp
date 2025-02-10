382cf76ef0f969aaa9438fcc81f0406ee97981d1
Dirk Zimmermann <me@dirkz.com>
2025-02-10 11:27:15 +0100

    Add a VertexPipeline class

---
+++ main/dxutil/VertexPipeline.cpp
@@ -0,0 +1,6 @@
+#include "VertexPipeline.h"
+
+namespace dxultra
+{
+
+}
---
+++ main/dxutil/VertexPipeline.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "stdafx.h"
+
+namespace dxultra
+{
+
+struct VertexPipeline
+{
+};
+
+} // namespace dxultra
