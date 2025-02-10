7cc7246359b6d60711740f40263203ebc34439ad
Dirk Zimmermann <me@dirkz.com>
2025-02-10 11:37:56 +0100

    Have a layout input description

--- main/dxutil/VertexPipeline.cpp
+++ main/dxutil/VertexPipeline.cpp
@@ -1,6 +1,24 @@
 #include "VertexPipeline.h"
 
+#include "Vertex.h"
+
 namespace dxultra
 {
 
+D3D12_INPUT_LAYOUT_DESC VertexPipeline::InputLayoutDescription()
+{
+    constexpr UINT SemanticIndex = 0;
+    constexpr UINT InputSlot = 0;
+    constexpr UINT InstanceDataStepRate = 0;
+
+    constexpr D3D12_INPUT_ELEMENT_DESC inputDescriptions[] = {
+        {"POSITION", SemanticIndex, DXGI_FORMAT_R32G32B32_FLOAT, InputSlot,
+         offsetof(Vertex, Position), D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,
+         InstanceDataStepRate},
+        {"COLOR", SemanticIndex, DXGI_FORMAT_R32G32B32_FLOAT, InputSlot, offsetof(Vertex, Color),
+         D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, InstanceDataStepRate}};
+
+    return {inputDescriptions, _countof(inputDescriptions)};
 }
+
+} // namespace dxultra
--- main/dxutil/VertexPipeline.h
+++ main/dxutil/VertexPipeline.h
@@ -7,6 +7,7 @@ namespace dxultra
 
 struct VertexPipeline
 {
+    static D3D12_INPUT_LAYOUT_DESC InputLayoutDescription();
 };
 
 } // namespace dxultra
