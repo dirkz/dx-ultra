716411cac2da153a87e28cafa7351c0ea4a11011
Dirk Zimmermann <me@dirkz.com>
2025-02-10 20:30:48 +0100

    Constructors for vertices

--- main/dxutil/Vertex.h
+++ main/dxutil/Vertex.h
@@ -9,6 +9,16 @@ struct Vertex
 {
     XMFLOAT3 Position;
     XMFLOAT3 Color;
+
+    Vertex(float x, float y, float z, XMVECTORF32 color) : Position{x, y, z}, Color{color}
+    {
+    }
+
+    Vertex(double x, double y, double z, XMVECTORF32 color)
+        : Position{static_cast<float>(x), static_cast<float>(y), static_cast<float>(z)},
+          Color{color}
+    {
+    }
 };
 
 } // namespace dxultra
