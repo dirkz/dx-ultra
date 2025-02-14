7497f1677a40300034f7d5b2b497b66e87d2c0b3
Dirk Zimmermann <me@dirkz.com>
2025-02-14 10:02:33 +0100

    Depth stencil buffer format

--- main/dxutil/DepthStencilBuffer.h
+++ main/dxutil/DepthStencilBuffer.h
@@ -7,6 +7,13 @@
 namespace dxultra
 {
 
+/// <summary>
+/// The depth stencil format.
+/// <code>DXGI_FORMAT_D24_UNORM_S8_UINT</code> is a 24bit float
+/// between 0.0 and 1.0, while the stencil component is an 8bit unsigned integer.
+/// </summary>
+constexpr DXGI_FORMAT DepthStencilFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+
 struct DepthStencilBuffer
 {
     DepthStencilBuffer(ComPtr<ID3D12Device4> device);
